8eb1a4150ad1b342349972165640d0c50e57d3c2
bdosari <aldawsar004@knights.gannon.edu>
2015-03-01 21:55:42 -0500

    some changes

--- test.java
+++ test.java
@@ -5,8 +5,10 @@ public class test {
     public static void main(String[] args) {
        
 		int a=0;
-	   
-	    System.out.println("Hello, World " + a);
+	    int c=5;
+		
+		
+	    System.out.println("This is a test message " + (a + c));
 		
     }
 
